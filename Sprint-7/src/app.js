7c9ac37fa6aa141942f42fd257e01cc35f73cccb
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-07-25 01:59:54 -0400

    por terminar 2.0

--- Sprint-7/src/app.js
+++ Sprint-7/src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const connectDB = require('./db');
 const eventoRouter = require('./router/eventoRouter');
 const customerRouter = require('./router/customerRouter');
+const autenticacionRouter = require('./router/autenticacionRouter');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./api-endpoints.json')
@@ -13,6 +15,10 @@ const app = express();
 
 app.use(cors());
 
+app.use(cookieParser());
+
+app.use(express.json());
+
 // Middleware para parsear los datos de solicitud como JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,12 +26,16 @@ app.use(bodyParser.json());
 // Conectar a la base de datos
 connectDB();
 
-//http://localhost:9095/api-docs/#/default
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Asociar las rutas del enrutador 'eventoRouter' y 'customerRouter'a la ruta '/api'
 app.use("/api", eventoRouter);
 app.use("/api", customerRouter);
+app.use("/api", autenticacionRouter);
+
+
+//http://localhost:9095/api-docs/#/default
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 // Iniciar el servidor y escuchar en el puerto 9095
 app.listen(9095, () => {
--- Sprint-7/src/router/autenticacionRouter.js
+++ Sprint-7/src/router/autenticacionRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const customerController = require('../controllers/customerController');
+const {inicioSesionUsuario, cerrarSesionUsuario} = require('../controllers/customer.controller');
 
-router.post('/login', customerController.inicioSesionUsuario);
-router.post('/logout', customerController.cerrarSesionUsuario);
+
+
+router.post('/login', inicioSesionUsuario);
+router.post('/logout', cerrarSesionUsuario);
 
 
 module.exports = router;
