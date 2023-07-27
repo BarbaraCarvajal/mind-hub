27be18d98c66fee85e55a85c95ca6edae15cd6ce
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-07-27 02:34:06 -0400

    register al 100%

--- Sprint-7/frontend/amazing-events-react-vite-barbara-carvajal/src/pages/Register.jsx
+++ Sprint-7/frontend/amazing-events-react-vite-barbara-carvajal/src/pages/Register.jsx
@@ -2,19 +2,22 @@ import React, { useRef, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
 const Register = () => {
+  // Referencias a los campos del formulario
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
   const rolRef = useRef(); 
 
+  // Estado para manejar mensajes de error
   const [error, setError] = useState("");
 
+  // Manejar el evento de registro
   const handleRegister = () => {
     setError("");
 
+    // Obtener los valores de los campos del formulario
     const name = nameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
@@ -32,16 +35,19 @@ const Register = () => {
       return;
     }
 
+    // Crear objeto de datos de usuario a enviar en la solicitud POST
     const userData = { name, email, password, rol };
 
+    // Realizar solicitud POST para registrar un nuevo usuario
     axios.post("http://localhost:9095/api/usuarios", userData)
       .then((response) => {
         console.log(response.data);
-        setSuccessMessage("Registro exitoso. Redirigiendo a la página de inicio...");
+        // Mostrar mensaje de registro exitoso
+        setError("Registro exitoso. Redirigiendo a la página de inicio...");
 
         // Redirigir a la página de login después de 2 segundos
         setTimeout(() => {
-          history.push("/login"); // Ruta de la página de inicio
+          window.location.href = "/login"; // Ruta de la página de inicio de sesión
         }, 2000);
       })
       .catch((error) => {
@@ -81,7 +87,7 @@ const Register = () => {
                 Register
               </button>
               <p className="mt-2">
-                Already have an account? <Link to="/login">Login</Link>
+                ¿Ya estás registrado? <Link to="/login">Login</Link>
               </p>
               <br />
               <br />
@@ -94,71 +100,3 @@ const Register = () => {
 };
 
 export default Register;
-
-
-/* import React, { useRef } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-
-const Register = () => {
-  const nameRef = useRef();
-  const passwordRef = useRef();
-  const emailRef = useRef();
-  const rolRef = useRef(); // Si también necesitas el campo "rol" en el formulario
-
-  const handleRegister = () => {
-    axios.defaults.withCredentials = true;
-
-    const userData = {
-      name: nameRef.current.value,
-      password: passwordRef.current.value,
-      email: emailRef.current.value,
-      rol: rolRef.current.value // Si también necesitas el campo "rol" en el formulario
-    };
-
-    axios.post("http://localhost:9095/api/register", userData)
-      .then((response) => {
-        console.log(response.data);
-        // Manejar el registro exitoso aquí (redirección, mensaje, etc.)
-      })
-      .catch((error) => {
-        console.log(error);
-        // Manejar el error en el registro aquí (mensaje de error, etc.)
-      });
-  };
-
-  return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" ref={nameRef} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="text" className="form-control" id="email" ref={emailRef} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" ref={passwordRef} />
-              </div>
-              <div className="form-group">
-                <label htmlFor="rol">Rol</label>
-                <input type="text" className="form-control" id="rol" ref={rolRef} />
-              </div> 
-              <button className="btn btn-primary" onClick={handleRegister}>
-                Register
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Register;
- */
